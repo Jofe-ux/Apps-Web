33dd3df19da62a7a15eb77667663cdde3e3be937
Jofe-ux <129334030+Jofe-ux@users.noreply.github.com>
2026-02-18 23:27:29 -0400

    Add files via upload

---
+++ entorno-backend/src/main/java/com/example/entornobackend/EntornoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.entornobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntornoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntornoBackendApplication.class, args);
+    }
+
+}
---
+++ entorno-backend/src/test/java/com/example/entornobackend/EntornoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.entornobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntornoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
